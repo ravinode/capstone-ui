8baa0ebacdbdb728b18496e32ee992e9a6906f9b
Ravi D <kira@Ravis-MacBook-Pro.local>
2017-08-17 13:33:10 -0400

    Fixed filter

--- src/app/pipe/filter-pipe.ts
+++ src/app/pipe/filter-pipe.ts
@@ -5,18 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 
 export class FilterPipe implements PipeTransform {
+    filterList = [];
     transform(value: any, filterString: string): any {
         if (value.length === 0 || filterString === 'empty' ) {
             console.log('hy' + value);
                 return value;
             }
         const resultArray = [];
+        this.filterList.push(filterString);
+        console.log('Resulttt' + this.filterList);
         for (const item of value) {
-            if (item.category === filterString ) {
+            for (const list of this.filterList)
+                {
+                if (item.category === list) {
                     resultArray.push(item);
-                    console.log(resultArray);
                 }
         }
+    }
         return resultArray;
     }
 }
